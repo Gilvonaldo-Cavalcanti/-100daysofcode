1a33a25902a329e3060e883e69d776b5fa635633
Gilvonaldo A. S. Cavalcanti <gilvonaldocavalcanti@gmail.com>
2019-10-16 22:47:10 -0300

    Implementação da aba progresso, adição de um calendário (Décimo terceiro dia)

--- app/src/app/pages/tab2/tab2.module.ts
+++ app/src/app/pages/tab2/tab2.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 
 import { Tab2Page } from './tab2.page';
 
+import { NgCalendarModule } from 'ionic2-calendar';
+
 const routes: Routes = [
   {
     path: '',
@@ -19,7 +21,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    NgCalendarModule
   ],
   declarations: [Tab2Page]
 })
--- app/src/app/pages/tab2/tab2.page.ts
+++ app/src/app/pages/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CalendarComponent } from 'ionic2-calendar/calendar';
 @Component({
   selector: 'app-tab2',
   templateUrl: './tab2.page.html',
@@ -7,9 +7,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab2Page implements OnInit {
 
+  event = {
+    title: '',
+    desc: '',
+    startTime: '',
+    endTime: '', 
+    allDay: false
+  };
+
+  minDate = new Date().toISOString();
+
+  eventSource = [];
+
+  calendar = {
+    mode: 'month',
+    currentDate: new Date()
+  }
+
+  viewTitle = '';
+
+  @ViewChild(CalendarComponent, { static: false }) myCal: CalendarComponent;
+
   constructor() { }
 
   ngOnInit() {
+    this.resetEvent();
   }
+
+  resetEvent(){
+    this.event = {
+      title: '',
+      desc: '',
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
+      allDay: false
+    };
+  }
+
+  addEvent(){
+    let eventCopy = {
+      title: this.event.title,
+      startTime: new Date(this.event.startTime),
+      endTime: new Date(this.event.endTime),
+      allDay: this.event.allDay,
+      desc: this.event.desc
+    };
+
+    if (eventCopy.allDay) {
+      let start = eventCopy.startTime;
+      let end = eventCopy.endTime;
+
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCDate(), start.getUTCMonth(), start.getUTCFullYear()));
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCDate() + 1, start.getUTCMonth(), start.getUTCFullYear()));
+    }
+
+    this.eventSource.push(eventCopy);
+    this.myCal.loadEvents();
+    this.resetEvent();
+
+  }
+
+
+  onTimeSelected(){
+
+  }
+
+  changeMode(mode){
+    this.calendar.mode = mode;
+  }
+
+  back(){
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slidePrev();
+  }
+
+  next(){
+    var swiper = document.querySelector('.swiper-container')['swiper'];
+    swiper.slideNext();
+  }
+
+  onViewTitleChanged(){
+
+  }
+
+  onEventSelected(){
+
+  }
+
 
 }
